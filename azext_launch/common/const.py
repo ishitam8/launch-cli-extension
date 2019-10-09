09a0d2a42486909f8ee299a966a04aa0515ce3d5
Ishita Mehta <ishitam@microsoft.com>
2019-10-09 14:14:32 +0530

    Integrating with final yaml pipeline

--- azext_launch/common/const.py
+++ azext_launch/common/const.py
@@ -1,9 +1,8 @@
 GITHUB_REQUEST_HEADERS = {'Content-Type': 'application/json' + '; charset=utf-8',
                 'Accept': 'application/json'}
 
-LAUNCH_AZURE_APP_SERVICE_URL = 'https://launchorgresolverapiappservice1.azurewebsites.net/'
+LAUNCH_AZURE_APP_SERVICE_URL = 'https://launchsvccliintegration.azurewebsites.net/'
 
-#LAUNCH_AZURE_APP_SERVICE_URL ='http://1cf6c3b8.ngrok.io/'
 
 LAUNCH_SERVICE_CLI_AUTH_CONFIG_URL = LAUNCH_AZURE_APP_SERVICE_URL+'ConfigureCLIAuth'
 LAUNCH_ORG_PIPELINE_SETUP = LAUNCH_AZURE_APP_SERVICE_URL + 'LaunchPipelineUrlresolver'
